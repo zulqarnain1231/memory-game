5db71ce0843e2c21727c9beeddff3a85f15f2dfe
zulqarnain1231 <zulqarnainniazi447@gmail.com>
2023-08-22 00:25:33 -0700

    created folder structure

---
+++ components/Cards/Card.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { IMemoryCard } from "../../types/types";
+
+type ICardProps = {
+  card: IMemoryCard;
+  onClick: (card: IMemoryCard) => void;
+  disabled: boolean;
+};
+
+const Card: React.FC<ICardProps> = ({ card, onClick, disabled }) => {
+  const className = `${card.isFlipped ? "flipped" : ""}`;
+
+  const handleClick = () => {
+    if (!disabled) {
+      onClick(card);
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className={className}>
+        <img className="front" src={card.image} alt={card.name} />
+        <img
+          className="back"
+          src="/img/cover.png"
+          alt="Card Cover"
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
